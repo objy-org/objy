a11e19326886a07125c16be8ca8a2d1e9be00f44
4ty711 <53232140+4ty711@users.noreply.github.com>
2020-11-04 16:15:23 +0100

    added test cases

--- test/test.js
+++ test/test.js
@@ -4,7 +4,10 @@ var assert = require('assert');
 
 var Obj;
 
+
 describe('Object', function() {
+    
+    // Basic
     test('should form an object', function(done) {
       
       OBJY.define({
@@ -17,4 +20,31 @@ describe('Object', function() {
       done();
       
     });
+
+
+  	// Inheritance
+    test('should inherit form object', function(done) {
+      
+   	  OBJY.Object({name: "template", properties: {
+   	  	weight: {
+   	  		type: "number",
+   	  		value: 23
+   	  	}, 
+   	  	age: 22
+   	  }}).add(data => {
+   	  	 OBJY.Object({inherits: [data._id]}).add(data => {
+
+   	  	 	OBJY.Object(data).get(obj => {
+   	  	 		console.log(obj)
+   	  	 		expect(obj.properties.weight.value).toBe(23);
+   	  	 		expect(obj.properties.age).toBe(22);
+   	  	 		done();
+   	  	 	})
+	   	  })
+   	  })
+    });
+
+
 });
+
+
