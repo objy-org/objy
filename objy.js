de4955bf65d512506bbf372f2d55c3d90ebf3664
Benjamin Lotterer <theedgesofben@hotmail.com>
2024-01-21 20:43:05 +0100

    Update objy.js

--- objy.js
+++ objy.js
@@ -6,58 +6,55 @@ if (_nodejs) {
     };
 }
 
-var _OBJY = {};
-
 var isObject = function(a) {
     return (!!a) && (a.constructor === Object);
 };
 
-
-var generalAttributes = require('./instance/attributes.js')(_OBJY)
-var generalFunctions = require('./instance/general.js')(_OBJY)
-var applyFunctions = require('./instance/apply.js')(_OBJY)
-var permissionFunctions = require('./instance/permission.js')(_OBJY)
-var objectFunctions = require('./instance/object.js')(_OBJY)
-var mapperFunctions = require('./instance/mapper.js')(_OBJY)
-var wrapperFunctions = require('./instance/wrapper.js')(_OBJY)
-var propertyFunctions = require('./instance/property.js')(_OBJY)
-var pluralConstructorFunctions = require('./instance/plural-constructor.js')(_OBJY)
-var singularConstructorFunctions = require('./instance/singular-constructor.js')(_OBJY)
+var generalAttributes = require('./instance/attributes.js')
+var generalFunctions = require('./instance/general.js')
+var applyFunctions = require('./instance/apply.js')
+var permissionFunctions = require('./instance/permission.js')
+var objectFunctions = require('./instance/object.js')
+var mapperFunctions = require('./instance/mapper.js')
+var wrapperFunctions = require('./instance/wrapper.js')
+var propertyFunctions = require('./instance/property.js')
+var pluralConstructorFunctions = require('./instance/plural-constructor.js')
+var singularConstructorFunctions = require('./instance/singular-constructor.js')
 
 /**
- * Main _OBJY Object
+ * OBJY Instance
  */
-Object.assign(_OBJY, {
-
-    ...generalAttributes,
-
-    ...generalFunctions,
-
-    ...applyFunctions,
-
-    ...permissionFunctions,
-
-    ...objectFunctions,
-
-    ...mapperFunctions,
-
-    ...wrapperFunctions,
-
-    ...propertyFunctions,
-
-    ...pluralConstructorFunctions,
-
-    ...singularConstructorFunctions,
-
-    hello: function() {
-        OBJY.Logger.log("Hello from OBJY!");
-    }
-})
-
-
-
 var OBJY = function(){
-    return Object.assign({}, _OBJY);
+    var _OBJY = {};
+
+    Object.assign(_OBJY, {
+
+        ...generalAttributes(_OBJY),
+    
+        ...generalFunctions(_OBJY),
+    
+        ...applyFunctions(_OBJY),
+    
+        ...permissionFunctions(_OBJY),
+    
+        ...objectFunctions(_OBJY),
+    
+        ...mapperFunctions(_OBJY),
+    
+        ...wrapperFunctions(_OBJY),
+    
+        ...propertyFunctions(_OBJY),
+    
+        ...pluralConstructorFunctions(_OBJY),
+    
+        ...singularConstructorFunctions(_OBJY),
+    
+        hello: function() {
+            OBJY.Logger.log("Hello from OBJY!");
+        }
+    })
+
+    return _OBJY
 }
 
 var objy = OBJY;
